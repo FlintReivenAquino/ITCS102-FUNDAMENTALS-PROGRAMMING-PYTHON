8ae6c081c839ea4541e34500e2e955125640a46f
FlintReivenAquino <2pesotakoyaki0@gmail.com>
2025-08-15 01:38:12 -0700

    Update Activity5.py

--- Activity5.py
+++ Activity5.py
@@ -1,7 +1,8 @@
-something =eval(input("input something --> "))
+something =eval(input("pick a number and i'll add 10 --> "))
 
 print (" The data type of something is ", type(something))
 
-answer = 6 + something
+answer = 10 + something
+
 
 print("the answer is ", answer)
