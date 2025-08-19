2420ac8b1d99e18695c1c1e065b5bf36a7c0d6de
FlintReivenAquino <2pesotakoyaki0@gmail.com>
2025-08-20 01:37:50 +0800

    Add files via upload

---
+++ Activity9.py
@@ -0,0 +1,7 @@
+username = 'Flint'
+password = '2pesotakoyaki'
+
+print(username == 'Flint')
+print((username == 'Flint') and (password == '2pesotakoyaki'))
+print((username == 'Flint') or (password == '2pesotakoyaki'))
+print (not((username == 'Flint') or (password == '2pesotakoyaki')))
