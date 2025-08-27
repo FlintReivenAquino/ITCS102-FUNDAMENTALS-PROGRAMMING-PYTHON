115379eab57e8f3abcfa5b1221628bde58909259
FlintReivenAquino <2pesotakoyaki0@gmail.com>
2025-08-27 11:25:05 +0800

    Add files via upload

---
+++ Code_Challenge3.py
@@ -0,0 +1,14 @@
+username = 'Flint'
+password = '2pesotakoyaki'
+
+
+name = input("Enter your username ->")
+password = input ("Enter your password ->")
+
+if username == username and password == password :
+     print("Access granted")
+else:
+     print("Access Denied")
+
+
+
---
+++ Code_Challenge4.py
@@ -0,0 +1,10 @@
+number = eval(input("input any number -->"))
+
+
+if number %2 == 0:
+
+     print(number, "even")
+
+else:
+
+     print(number, "odd")
