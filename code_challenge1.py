bf0b926a1ac466968b4a39315a1e522534378d80
FlintReivenAquino <2pesotakoyaki0@gmail.com>
2025-09-10 02:33:56 -0700

    Update and rename codechallenge_1.py to code_challenge1.py

---
+++ code_challenge1.py
@@ -0,0 +1,3 @@
+name = input ("what is your name .? ")
+
+print("\t\t\t\t\t*\n\n\n\t\t\t\t*\t\t*\n\n\n\t\t\t*\t\t\t\t*\n\n\n\t\t*\t\t\t\t\t\t*\n\n\t\t\t\t     " , name , "\n\t*\t\t\t\t\t\t\t\t*\n\t\t\t\t      BSIT-1A\n\n\t\t*\t\t\t\t\t\t*\n\n\n\t\t\t*\t\t\t\t*\n\n\n\t\t\t\t*\t\t*\n\n\n\t\t\t\t\t*\n\t\t\t\t\t")
--- codechallenge_1.py
+++
@@ -1,3 +0,0 @@
-name = input ("what is your name .? ")
-
-print("\t\t\t\t\t*\n\n\n\t\t\t\t*\t\t*\n\n\n\t\t\t*\t\t\t\t*\n\n\n\t\t*\t\t\t\t\t\t*\n\n\t\t\t\t     " , name , "\n\t*\t\t\t\t\t\t\t\t*\n\t\t\t\t      BSIT-1A\n\n\t\t*\t\t\t\t\t\t*\n\n\n\t\t\t*\t\t\t\t*\n\n\n\t\t\t\t*\t\t*\n\n\n\t\t\t\t\t*\n\t\t\t\t\t")
