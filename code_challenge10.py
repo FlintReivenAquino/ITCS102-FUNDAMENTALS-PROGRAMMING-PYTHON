7a1383f967bf96370910249d0b5aef9b95dd568a
FlintReivenAquino <2pesotakoyaki0@gmail.com>
2025-09-25 22:03:43 +0800

    Add files via upload

---
+++ code_challenge10.py
@@ -0,0 +1,12 @@
+for x in range(10,0,-1):
+    
+    for q in range(1,x,1):
+        print(" ", end=" ")
+   
+    for w in range(10,x,-1):
+        print("*",end=" ")
+    
+    for e in range(11,x,-1):
+   
+        print("*",end=" ")
+    print()
---
+++ code_challenge11.py
@@ -0,0 +1,22 @@
+for x in range(10,0,-1):
+    
+    for q in range(1,x,1):
+        print(" ", end=" ")
+    
+    for w in range(10,x,-1):
+        print("*",end=" ")
+    
+    for e in range(11,x,-1):
+        print("*",end=" ")
+    print()
+
+for z in range (1,11,1):
+    for a in range(1,z,1):
+        print(" ",end=" ")
+    
+    for s in range(10,z,-1):
+        print("*",end=" ")
+    
+    for d in range(11,z,-1):
+        print("*",end=" ")
+    print()
